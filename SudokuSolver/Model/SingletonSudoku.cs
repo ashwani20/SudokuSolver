99c73090aa9f4b798b2ba6020896561af66581e4
ashwani20 <k.ashwani20@gmail.com>
2020-03-04 21:45:27 -0500

    Add project files.

---
+++ SudokuSolver/Model/SingletonSudoku.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.Model
+{
+    class SingletonSudoku
+    {
+        //private static SudokuSingletonClass instance = null;
+        private static int[,] sudokuBoard = null;
+        private SingletonSudoku() { }
+
+        public int[,] getSingletonSudokuBoard(int n) {
+            if (sudokuBoard == null)
+                sudokuBoard = new int[n,n];
+            return sudokuBoard;
+        }
+    }
+}
---
+++ SudokuSolver/Model/SudokuModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver.SudokuModel
+{
+    class SudokuModel
+    {
+        public static void main(string[] args) { 
+
+        }
+
+    }
+}
