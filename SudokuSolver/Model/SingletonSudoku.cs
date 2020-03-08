85c6fa3ab04635cfdd3fe008a4589148daa8929d
ashwani20 <k.ashwani20@gmail.com>
2020-03-07 22:36:38 -0500

    Added code for Model which has 2 classess- SingletonSudoku.cs and SudokuModel.cs. SingletonSudoku creates a singleton Sudoku board and SudokuModel has the backtrack logic for implementing a Sudoku. Also, added code for View - Sudoku.cs which has the code for printing solution for Sudoku solution and it's puzzle.

--- SudokuSolver/Model/SingletonSudoku.cs
+++ SudokuSolver/Model/SingletonSudoku.cs
@@ -1,18 +1,37 @@
-﻿using System;
+﻿/*
+ * CSCI 641 Project 2
+ * @author  Ashwani Kumar (ak8647)
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SudokuSolver.Model
 {
+    /// <summary>
+    /// This is a singleton class which creates a single instance of Sudoku board
+    /// </summary>
     class SingletonSudoku
     {
-        //private static SudokuSingletonClass instance = null;
         private static int[,] sudokuBoard = null;
+        private static int size;
         private SingletonSudoku() { }
+        /// <summary>
+        /// Property for size of sudokuBoard
+        /// </summary>
+        public static int Size { get => size; set => size = value; }
 
-        public int[,] getSingletonSudokuBoard(int n) {
+        /// <summary>
+        /// This is a static method which creates only 1 instance of 2D Sudoku board.
+        /// When called, it first checks if a 2D int sudokuBoard array exists. It it
+        /// does, then return the existing 2D array object. Otherwise, it creates a
+        /// new instance only 1 time.
+        /// </summary>
+        /// <returns>static instance of a 2D Sudoku board which of type int</returns>
+        public static int[,] GetSingletonSudokuBoard() {
             if (sudokuBoard == null)
-                sudokuBoard = new int[n,n];
+                sudokuBoard = new int[Size,Size];
             return sudokuBoard;
         }
     }
---
+++ SudokuSolver/View/Sudoku.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * CSCI 641 Project 2
+ * @author  Ashwani Kumar (ak8647)
+ */
+
+using SudokuSolver.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SudokuSolver.View
+{
+    /// <summary>
+    /// The main View of the Sudoku puzzle. It takes 2 command line arguments where first
+    /// argument is the size of the Sudoku and second argument is the difficulty type of the
+    /// puzzle.
+    /// </summary>
+    class Sudoku
+    {
+        /// <summary>
+        /// The main entry point of the SudokuSolver console application.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args) {
+            if (args.Length > 1)
+            {
+                SudokuModel obj = new SudokuModel(Int32.Parse(args[0]), args[1]);
+                
+                showSudoku(SingletonSudoku.GetSingletonSudokuBoard());
+                obj.CreateSudokuPuzzle(SingletonSudoku.GetSingletonSudokuBoard());
+                Console.WriteLine();
+                showSudoku(SingletonSudoku.GetSingletonSudokuBoard());
+            }
+            else
+            {
+                Console.WriteLine("Invalid command line arguments. A valid command line arguments look " +
+                    "like - 4 easy");
+                Console.WriteLine("The first argument is size of Sudoku puzzle and the second argument " +
+                    "is the difficulty level.");
+            }
+        }
+
+        /// <summary>
+        /// Beautifies the sudoku and displays each entry. The solution for puzzle is displaced
+        /// as it is. However, puzzle contains "X" for displaying blanks.
+        /// </summary>
+        /// <param name="sudokuBoard">2D singleton int array of Sudoku board</param>
+        public static void showSudoku(int[,] sudokuBoard)
+        {
+            int len = (int)Math.Sqrt(sudokuBoard.Length);
+            int gridSize = (int)Math.Sqrt(len);
+            string sudokuStr;
+            for (int i = 0; i < len; i++)
+            {
+                if (i % gridSize == 0)
+                    printLine(len, gridSize);
+                sudokuStr = "";
+                for (int j = 0; j < len; j++)
+                {
+                    String eachChar = sudokuBoard[i, j] == 0 ? "X" : sudokuBoard[i, j].ToString();
+                    if (j % gridSize == 0)
+                        sudokuStr += "| ";
+                    sudokuStr += eachChar + " ";
+                }
+                sudokuStr += "|";
+                Console.WriteLine(sudokuStr);
+            }
+            printLine(len, gridSize);
+        }
+        /// <summary>
+        /// Displays a beautified horizontal rule
+        /// </summary>
+        /// <param name="len">the size of sudoku puzzle</param>
+        /// <param name="gridSize">the size of sudoku grid</param>
+        public static void printLine(int len, int gridSize)
+        {
+            string horizontalLine = "";
+            for (int i = 0; i < len; i++) 
+            {
+                if (i % gridSize == 0)
+                    horizontalLine += "+-";
+                horizontalLine += "--";
+            }
+            horizontalLine += "+";
+            Console.WriteLine(horizontalLine);
+        }
+    }
+}
